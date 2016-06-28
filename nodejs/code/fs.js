bf6f04278fbe36e667e1446d4a7afaf3f79158ed
ginger-k <ywkxwx@163.com>
2016-06-28 20:56:22 +0800

    update

--- nodejs/code/fs.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-var input = fs.createReadStream('./helloDir/big.txt');
-input.on('data', function(data){
-	console.log('------------------------------读取数据：' + data);
-});
-input.on('end', function(){
-	console.log('读取数据结束！');
-});
